0fb0a0ef91cc4799e48f869b47ede532feadad23
BersihSekali <iam.cleanklin@gmai.com>
2022-02-14 17:13:33 +0700

    14-02 17:12

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,12 +4,42 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
+    protected $builder, $db;
+
+    public function __construct()
+    {
+        $this->db = \Config\Database::connect();
+        $this->builder = $this->db->table('memo');
+    }
+
     public function index()
     {
+        $this->builder->selectCount('id_surat');
+        $total = $this->builder->get();
+
+        $this->builder->selectCount('status', 'noAction');
+        $this->builder->where('status', 0);
+        $noAction = $this->builder->get();
+
+        $this->builder->selectCount('status', 'progres');
+        $this->builder->where('status >=', 1);
+        $this->builder->where('status <=', 3);
+        $progres = $this->builder->get();
+        
+        $this->builder->selectCount('status', 'done');
+        $this->builder->where('status', 4);
+        $done = $this->builder->get();
+
         $data = [
             'title' => 'Beranda',
-            'judul' => ''
+            'judul' => '',
+            'total' => $total->getResult(),
+            'noAction' => $noAction->getResult(),
+            'progres' => $progres->getResult(),
+            'done' => $done->getResult()
         ];
+
+        // dd($data);
         return view('templates/vHome', $data);
     }
 }
--- app/Views/templates/vHome.php
+++ app/Views/templates/vHome.php
@@ -18,4 +18,60 @@
     ?>
 
     <h1>Selamat <?= $salam; ?></h1>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-header bg-primary">
+                    </div>
+                        
+                    <div class="card-body">
+                        <h5>Total Surat</h5>
+                        <i class="fas fa-copy fa-2xl"></i>
+                        <span><?= $total[0]->id_surat; ?> </span>
+                    </div>
+                </div>
+            </div>    
+
+            <div class="col">
+                <div class="card">
+                    <div class="card-header bg-success">
+                    </div>
+                        
+                    <div class="card-body">
+                        <h5>Selesai</h5>
+                        <i class="fas fa-clipboard-check fa-2xl"></i>
+                        <span> <?= $done[0]->done; ?> </span>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col">
+                <div class="card">
+                    <div class="card-header bg-warning">
+                    </div>
+                        
+                    <div class="card-body">
+                        <h5>Progres</h5>
+                        <i class="fas fa-spinner fa-spin fa-2xl"></i>
+                        <span> <?= $progres[0]->progres; ?> </span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card">
+                    <div class="card-header bg-dark">
+                    </div>
+                        
+                    <div class="card-body">
+                        <h5>No Action</h5>
+                        <i class="fa-solid fa-circle-xmark fa-beat fa-2xl"></i>
+                        <span> <?= $noAction[0]->noAction; ?> </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 <?= $this->endSection(); ?>
